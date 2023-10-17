9246f767601e50936c82b283678775efc549e1a7
Emily <58041173+overestimate@users.noreply.github.com>
2023-10-16 23:14:29 -0500

    add new fetchy mode for elements breaks anything using an old version but no-one depends on this yet so

--- src/fetchy.js
+++ src/fetchy.js
@@ -1,39 +1,51 @@
+/* modified version of fetchy, to be merged into main. */
+
 /* fetchy-loader (c) 2023- emily lawrence. licensed under MIT license */
 
-let fetchy_loader = {
-    _debug: false, // do we call debugger on fatal errors?
+let Fetchy = {
+
+    debug: false, // do we call debugger on fatal errors?
+    navigate_anyways: true,
+
+    _new_url: "",
 
     _error: (...data) => {
         console.error('fetchy-loader: %cerror:%c',
-            'color: red; font-weight: 700;', 'color: revert; font-weight: revert;', ...data)
-        if (fetchy_loader._debug) { debugger }
+            'color: red; font-weight: 700;', 'color: revert; font-weight: revert;', ...data);
+        if (Fetchy.debug) { debugger; }
     },
     _warn: (...data) => {
         console.error('fetchy-loader: %cwarning:%c',
-            'color: red; font-weight: 500;', 'color: revert; font-weight: revert;', ...data)
+            'color: red; font-weight: 500;', 'color: revert; font-weight: revert;', ...data);
     },
-    _info: (...data) => { console.info('fetchy-loader: %cinfo:%c', 'color: lightblue;', 'color: revert;', ...data) },
-    _debug: (...data) => { console.debug('fetchy-loader: %debug:%c', 'color: green;', 'color: revert;', ...data) },
+    _info: (...data) => { console.info('fetchy-loader: %cinfo:%c', 'color: lightblue;', 'color: revert;', ...data); },
+    _debug: (...data) => { console.debug('fetchy-loader: %debug:%c', 'color: green;', 'color: revert;', ...data); },
 
-    /* url is the url you want to load.
-    * init is the init object for fetch, used for setting headers/cookies/etc.
-    * if navigate_anyways is true, then we navigate to the location on failure.
-    */
-    load: (url, init, navigate_anyways) => {
-        let new_url = new URL(url, document.location); // we do this to allow relative urls
-        fetch(new_url, init).then(response => {
-            response.text().then((text) => { // text is a promise so we have to do this
-                document.write(text); // load new page by writing html to document (ideally i'd load it and hotswap)
-                window.history.pushState({"html": text, "pageTitle": document.title}, "", new_url); // push new url to history
-            }, (reason) => fetchy_loader.navigate_failed(reason) ).catch((e) => { 
-                if (navigate_anyways === true) { document.location = new_url } // nav if failed
-                fetchy_loader._error(e)
-            })
-        }).catch((e => {
-            if (navigate_anyways === true) { document.location = new_url } // nav if failed
-            fetchy_loader._error(e)
-        }))
+    _catch_fail_fn: (e) => {
+        Fetchy._error(e);
+    },
+    _navigate_failed: (reason) => Fetchy._warn(reason),
+
+    // TODO: reimplement load()
+    
+    // returns a div containing the content at the url provided.
+    get_html: async (url, opt) => {
+        let new_url = new URL(url, document.location)
+        let res = await fetch(new_url, opt)
+        let text_html = await res.text();
+        let donor_element = document.createElement("div");
+        donor_element.innerHTML = text_html;
+        return donor_element;
     },
 
-    navigate_failed: (reason) => fetchy_loader._warn(reason)
+    doc_onload: (e) => {
+        document.querySelectorAll("*[data-source-url]").forEach(async (v, k) => {
+            let url = v.getAttribute("data-source-url");
+            v.innerHTML = (await Fetchy.get_html(url)).innerHTML;
+        })
+    }
+}
+
+window.onload = (e) => {
+    Fetchy.doc_onload(e);
 }
